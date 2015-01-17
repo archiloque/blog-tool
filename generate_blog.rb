0e430ef74f4e0393b66fa62567eec4c3f56659dd
Julien Kirch <code@archiloque.net>
2015-01-17 14:16:43 +0100

    Code improvements

--- generate_blog.rb
+++ generate_blog.rb
@@ -16,16 +16,41 @@ unless Dir.exist? BLOG_TARGET_PATH
   Dir.mkdir BLOG_TARGET_PATH
 end
 
+class Author
+
+  attr_reader :name, :twitter_handle, :fb_url, :main_url
+
+  def initialize(name, author)
+    @twitter_handle = author['twitter_handle']
+    @fb_url = author['fb_url']
+    @main_url = author['main_url']
+    @name = name
+  end
+end
+
+
+AUTHORS = {}
+JSON.parse(File.read('authors.json')).collect do |name, value|
+  author = Author.new(name, value)
+  if value['default_author']
+    DEFAULT_AUTHOR = author
+  end
+  AUTHORS[name] = author
+end
+
 ARTICLES = []
-AUTHORS = JSON.parse(File.read('authors.json'))
+
 class Article
 
-  attr_reader :dir_name, :document, :source_dir
+  MONTHS = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre']
+
+  attr_reader :dir_name, :document, :source_dir, :date
 
   def initialize(dir_name, document, source_dir)
     @dir_name = dir_name
     @document = document
     @source_dir =source_dir
+    @date = Date.parse(document.revdate)
   end
 
   def title
@@ -40,16 +65,20 @@ class Article
     document.author
   end
 
-  def date
-    document.revdate
-  end
-
   def image
     document.attributes['article_image']
   end
 
   def content
     document.render
+  end
+
+  def ignore_files
+    (document.attributes['ignore_files'] || '').split(',').collect{|i| i.strip}
+  end
+
+  def formatted_date
+    "#{date.day} #{MONTHS[date.month]} #{date.year}"
   end
 
 end
@@ -86,8 +115,8 @@ def copy_if_different(source, target)
   end
 end
 
+# Render main page
 main_template = Tilt::ERBTemplate.new('templates/main.erb.html', :default_encoding => 'UTF-8')
-# Render article
 main_target_file = File.join(BLOG_TARGET_PATH, 'index.html')
 p "Rendering [#{main_target_file}]"
 File.open(main_target_file, 'w') do |file|
@@ -95,10 +124,12 @@ File.open(main_target_file, 'w') do |file|
       main_template.render(
           Object.new,
           {
-              :articles => ARTICLES,
+              :articles => ARTICLES.sort_by!{|article| article.date}.reverse,
               :blog_root_url => BLOG_ROOT_URL,
+              :author => DEFAULT_AUTHOR
           }))
 end
+
 
 article_template = Tilt::ERBTemplate.new('templates/article.erb.html', :default_encoding => 'UTF-8')
 
@@ -112,6 +143,7 @@ ARTICLES.each do |article|
   # Render article
   target_file = File.join(article_target_dir, 'index.html')
   p "Rendering [#{target_file}]"
+  article_author = AUTHORS[article.author]
   File.open(target_file, 'w') do |file|
     file.puts(
         article_template.render(
@@ -121,11 +153,8 @@ ARTICLES.each do |article|
                 :article_root_url => BLOG_ROOT_URL + article.dir_name + '/',
                 :article_content => article.content,
                 :article_title => article.title,
-                :article_author => article.author,
-                :author_url => AUTHORS[article.author]['main_url'],
-                :author_fb_url => AUTHORS[article.author]['fb_url'],
-                :author_twitter_url => AUTHORS[article.author]['twitter_url'],
-                :article_date => article.date,
+                :author => article_author,
+                :article_date => article.formatted_date,
                 :article_description => article.description,
                 :article_image => article.image
             }))
@@ -134,16 +163,17 @@ ARTICLES.each do |article|
   # Copy other files
   existing_files = Dir.glob(File.join(article_target_dir, '*')).collect { |f| File.basename(f) }
   existing_files -= ['index.html']
+  ignore_files = article.ignore_files + [BLOG_ARTICLE_BASE_NAME]
   Dir.glob(File.join(article.source_dir, '*')).each do |attached_file_source|
     attached_file_base_name = File.basename(attached_file_source)
-    unless attached_file_base_name == BLOG_ARTICLE_BASE_NAME
+    unless ignore_files.include? attached_file_base_name
       attached_file_target = File.join(article_target_dir, attached_file_base_name)
       copy_if_different(attached_file_source, attached_file_target)
       existing_files -= [attached_file_base_name]
     end
   end
 
-  # Delete unused file
+  # Delete unused files
   existing_files.each do |file_to_delete|
     file_to_delete_with_path = File.join(article_target_dir, file_to_delete)
     p "Deleting [#{file_to_delete_with_path}]"
